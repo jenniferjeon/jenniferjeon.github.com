afb597833a4477ad6f4ba7498e535a9fe1add542
jenniferjeon <jljeon@ucdavis.edu>
2021-02-02 15:56:51 -0800

    2nd

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -2,34 +2,41 @@
 
     'use strict';
 
-    var myForm = document.querySelector('#myForm');
-    var madlib = document.querySelector('#madlib');
+    const myForm = document.querySelector('#myForm');
+    const madlib = document.querySelector('#madlib');
 
 
     myForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
 
-        var name = document.querySelector('#name').value;
-        var job = document.querySelector('#job').value;
-        var adverb = document.querySelector('#adverb').value;
-        var adjective1 = document.querySelector('#adjective1').value;
-        var adjective2 = document.querySelector('#adjective2').value;
-        var company = document.querySelector('#company').value;
+        const name = document.querySelector('#name').value;
+        const job = document.querySelector('#job').value;
+        const adverb = document.querySelector('#adverb').value;
+        const adjective1 = document.querySelector('#adjective1').value;
+        const adjective2 = document.querySelector('#adjective2').value;
+        const company = document.querySelector('#company').value;
 
         if (name && job && adverb && adjective1 && adjective2 && company) {
-            var myText = `Dear ${name}, Thank you so much for applying for the ${job} position. While we enjoyed getting to know you ${adverb}, with your ${adjective1} qualifications, we decided to move forward with a ${adjective2} candidate. We wish you the best in your future endeavors.
-            Best,
-            ${company}`;
+            let myText = `Dear ${name}, <br> Thank you so much for applying for the ${job} position. While we enjoyed getting to know you ${adverb}, with your ${adjective1} qualifications, we decided to move forward with a ${adjective2} candidate. We wish you the best in your future endeavors.
+            <br> Best,
+            <br> ${company}`;
+            madlib.innerHTML = myText;
+            madlib.className = "output";
+            document.body.style.backgroundImage = "url('images/background2.svg')";
+            document.getElementById('box').style.backgroundColor = "white";
+          
         }
         else {
-            var myText = "Please enter the correct words so that I can reject you properly";
+            let myText = "Please enter the correct words so that I can reject you properly";
+            madlib.innerHTML = myText;
+            madlib.className = "error";
         }
-        var formData = document.querySelectorAll("input[type=text]");
-        for (var eachField of formData) {
-            eachField.value = "";
-        }
-    madlib.innerHTML = myText;
+
+      //  const formData = document.querySelectorAll("input[type=text]");
+       /// for (let eachField of formData) {
+        //    eachField.value = "";
+       // }
     });
 
 
